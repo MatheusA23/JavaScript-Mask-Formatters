0ed5c3ef713b10bc6d69815210d35afb4549233a
Matheus Alves <133893712+MatheusA23@users.noreply.github.com>
2024-02-23 18:02:52 -0300

    Update CNPJ-Formatter.js

--- CNPJ-Formatter.js
+++ CNPJ-Formatter.js
@@ -1,16 +1,27 @@
 function formatCNPJ(cnpj) {
-    // Remover letras e manter apenas números
+    // Remover tudo que não for dígito
     const numerosCNPJ = cnpj.replace(/\D/g, '');
 
     // Limitar a quantidade de números a 14 caracteres
     const cnpjNumerosLimitados = numerosCNPJ.slice(0, 14);
 
-    // Aplicar a máscara
-    const cnpjRegex = /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/;
-    return cnpjNumerosLimitados.replace(cnpjRegex, '$1.$2.$3/$4-$5');
+    // Adicionar as pontuações em intervalos específicos
+    let cnpjFormatado = '';
+    for (let i = 0; i < cnpjNumerosLimitados.length; i++) {
+        cnpjFormatado += cnpjNumerosLimitados[i];
+        if (i === 1 || i === 4) {
+            cnpjFormatado += '.';
+        } else if (i === 7) {
+            cnpjFormatado += '/';
+        } else if (i === 11) {
+            cnpjFormatado += '-';
+        }
+    }
+
+    return cnpjFormatado;
 }
 
 // Exemplo de uso:
-const cnpj = "4736722342334245"; // Um CNPJ sem máscara com letras e mais de 14 dígitos
+const cnpj = "4736722218234235"; // Um CNPJ sem máscara incompleto
 const cnpjFormatado = formatCNPJ(cnpj);
 console.log(cnpjFormatado);
